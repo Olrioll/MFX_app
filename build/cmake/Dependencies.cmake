524594a50cdbaded8d6ed3390d5f9a5988d461e6
vkgood <vkmarketgoods@gmail.com>
2021-09-02 10:47:53 +0300

    Интегрировал QSuperMacros

---
+++ build/cmake/Dependencies.cmake
@@ -0,0 +1,2 @@
+set(QSUPERMACROS_REPOSITORY "https://github.com/OlivierLDff/QSuperMacros.git" CACHE STRING "QSuperMacros repository")
+set(QSUPERMACROS_TAG "89f02b5b2cceedc4d9a88519fa883cd8b93477f6" CACHE STRING "Git Tag of QSuperMacros")
